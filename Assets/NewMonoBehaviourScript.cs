11a0a0012e01dc48cd1ec74059105e3a940d8d2b
Jordan Pohr <97552850+jordanp2002@users.noreply.github.com>
2025-01-27 22:44:03 -0800

    Commit
    Commit

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
+
+    void Start()
+    {
+        Debug.Log("Calling the Start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S)) 
+        { 
+            inputVector += Vector2.down; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane);
+
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
+        Debug.Log("Calling the Update method");
+    }
+}
